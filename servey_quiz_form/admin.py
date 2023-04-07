bbc686de52fdad0d4b3ce9ad0dd7196176701035
13KuntalRoy <roykuntal1213@gmail.com>
2023-04-07 21:32:21 +0530

    link with admin form

--- servey_quiz_form/admin.py
+++ servey_quiz_form/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from payment.models import Choices,Questions,Answer,Form,Responses
+
+
+admin.site.register(Choices)
+
+admin.site.register(Questions)
+
+admin.site.register(Answer)
+
+admin.site.register(Form)
+
+admin.site.register(Responses)
