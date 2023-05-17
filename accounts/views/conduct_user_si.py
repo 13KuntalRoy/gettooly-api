02f113603bfe56aafec0e17ef4c9339ba370ae98
13KuntalRoy <roykuntal1213@gmail.com>
2023-05-17 20:59:11 +0530

    sub id update

--- accounts/views/conduct_user_si.py
+++ accounts/views/conduct_user_si.py
@@ -1,11 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from accounts.models import ConductUser
 from accounts.serializers import ConductUserSerializer
 
 class ConductUserSciAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (JWTAuthentication,)
     def put(self, request, pk):
         try:
             user = ConductUser.objects.get(id=self.request.user.id)
