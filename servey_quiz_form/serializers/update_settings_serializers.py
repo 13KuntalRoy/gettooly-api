d86e1d4f51623676d37839b9ad634513b81ff226
13KuntalRoy <roykuntal1213@gmail.com>
2023-05-27 21:16:07 +0530

    update formvalis in update settings

--- servey_quiz_form/serializers/update_settings_serializers.py
+++ servey_quiz_form/serializers/update_settings_serializers.py
@@ -3,4 +3,4 @@ from servey_quiz_form.models import Form
 class edit_settings(ModelSerializer):
     class Meta:
         model = Form
-        fields = ['collect_email','authenticated_responder','edit_after_submit','confirmation_message','is_quiz','allow_view_score','available_time','exam_duration']
+        fields = ['collect_email','authenticated_responder','edit_after_submit','confirmation_message','is_quiz','allow_view_score','available_time','exam_duration','form_valid']
